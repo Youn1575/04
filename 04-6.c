46ba1ea3cce58015a613dbc4ac7335dac61edbf6
youn1575 <youn461515@gmail.com>
2023-10-05 16:01:56 +0900

    04-6

---
+++ 04-6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int input;
+	int sec, min, hour;
+	
+	printf("input second : ");
+	scanf("%d", &input);
+	
+	sec = input%60;
+	min = (input%3600)/60;
+	hour = input/3600;
+	
+	printf("The time is %d : %d : %d \n", hour, min, sec);
+	
+	system("PAUSE");	
+	return 0;
+}
